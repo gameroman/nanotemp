6e823bf5b4de4fe87d131532f2061e8b1c8cb483
Roman <dev@rman.dev>
2026-03-09 01:02:54 +0000

    Update tsdown.config.ts

--- packages/quick-temp/tsdown.config.ts
+++ packages/quick-temp/tsdown.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   entry: ["src/index.ts", "src/mktemp.ts"],
   exports: true,
   dts: true,
+  outputOptions: { minifyInternalExports: false },
   format: ["esm", "cjs"],
 });
